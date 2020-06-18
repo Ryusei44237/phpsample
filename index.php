76a1333b617fa9941d01ed0ba5765adea0061f47
Ryusei44237 <r.nakamura.sys19@morijyobi.ac.jp>
2020-06-18 11:46:02 +0900

    add index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$dsn = 'mysql:dbname=user;host=127.0.0.1;';
+$user = 'Ryusei';
+$password = 'morijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $sql = "select * from user";
+    $result = $dbh->query($sql);
+    echo "接続成功\n";
+} catch (PDOException $e) {
+    echo "接続失敗: " . $e->getMessage() . "\n";
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index.php</title>
+</head>
+<body>
+<div class="container">
+    <?php foreach( $result as $value ){
+        echo "<h1>$value[id] $value[name] $value[age]</h1>";
+    }?>
+</div>
+</body>
+</html>
