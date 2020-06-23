64c12f0670ed64c90516887ab8edd87deca79c79
Ryusei44237 <r.nakamura.sys19@morijyobi.ac.jp>
2020-06-23 09:53:36 +0900

    commit

--- index.php
+++ index.php
@@ -12,18 +12,3 @@ try {
     exit();
 }
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index.php</title>
-</head>
-<body>
-<div class="container">
-    <?php foreach( $result as $value ){
-        echo "<h1>$value[id] $value[name] $value[age]</h1>";
-    }?>
-</div>
-</body>
-</html>
